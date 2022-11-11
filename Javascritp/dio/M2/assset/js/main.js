624ce7fc9a90c1bf085f6f4a41c1cd1177892ecc
limandersom <andersonelima@gmail.com>
2022-11-11 17:38:05 -0300

    Projeto Pokedex

---
+++ Javascritp/dio/M2/assset/js/main.js
@@ -0,0 +1,72 @@
+/**
+ * Enviando a requisição e como ela é assíncrona, ela retorna uma promessa de resposta, 
+ * checamos o retorno com o response.
+ * 
+ * CÓDIGO MUTIO GRANDE!
+ *  Para resolver isso usa-se
+ *  ARROW FUNCTION
+ *  Então substitui o código abaixo por:
+ */
+/* fetch(url)
+    .then(function (response) {
+        // console.log(response);
+
+        // * Convertendo o responseBody e .json 
+        // * mas ficou um .then() detro do outro.
+        // * Para evitar isso....
+        // response.json().then(function (responseBody) {
+        //     console.log(responseBody);            
+        // })
+        return response.json()
+    })
+    .then(function (jsonBody){
+        console.log(jsonBody);
+    })
+    .catch(function (error) {
+        console.error(error)
+    })
+    .finally(function () {
+        console.log('Requisição concluída!')
+    })
+ */
+
+function convertPokemonToLi(pokemon) {
+    return `
+    <li class="pokemon">
+        <span class="number">#122</span>
+        <span class="name">${pokemon.name}</span>
+
+        <div class="detail">
+            <ol class="types">
+                <li class="type">Psychic</li>
+                <li class="type">Fairy</li>
+            </ol>
+
+            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/122.svg"
+                alt="${pokemon.name}">
+        </div>
+    </li>    
+    `
+}
+
+const pokemonList = document.getElementById('pokemonsList')
+// pokemonList.innerHTML += '<li>Anderson teste</li>'
+
+/** REFATORADO O CÓDIGO USANDO A FUNÇÃO MAP
+ * para polupar o array antes de renderizar o HTML
+ *  
+pokeApi.getPokemons().then((pokemons) => {
+        //ele para o a exercução do código lá no devtools do browser.
+        //debugger
+        // console.log(pokemonList)
+
+        for (let i = 0; i < pokemons.length; i++) {
+            const pokemon = pokemons[i];
+            // console.log(convertPokemonToLi(pokemon));
+            pokemonList.innerHTML += convertPokemonToLi(pokemon) 
+        }
+    })*/
+
+pokeApi.getPokemons().then((pokemons = []) => {
+    pokemonList.innerHTML += pokemons.map(convertPokemonToLi).join('')
+})
