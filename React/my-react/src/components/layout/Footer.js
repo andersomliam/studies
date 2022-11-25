99de0c57460a61da999f98251a15b9b868463947
anderson <anderson@debian.debian>
2022-11-24 23:20:09 -0300

    16.5 - React Icons

--- React/my-react/src/components/layout/Footer.js
+++ React/my-react/src/components/layout/Footer.js
@@ -1,10 +1,26 @@
+import { FaFacebook, FaInstagram, FaLinkedin, FaLinkedinIn } from 'react-icons/fa'
+
+import style from './Footer.module.css'
+
 function Footer() {
 
     return (
-        <>
-            <hr></hr>
-            <p>Rodapé Aqui!</p>
-        </>
+        <footer>
+            <ul className={style.social_list}>
+                <li>
+                    <FaFacebook />
+                </li>
+                <li>
+                    <FaInstagram />
+                </li>
+                <li>
+                    <FaLinkedin />
+                </li>
+                <li>
+                    <FaLinkedinIn />
+                </li>
+            </ul>
+        </footer>
     )
 }
 
